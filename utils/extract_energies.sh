c80b87bf3bfe62867052a3ced443bfbed08661fc
tobiasma <marektobias@email.cz>
2025-11-11 22:10:15 +0100

    More robust energy extractions

--- utils/extract_energies.sh
+++ utils/extract_energies.sh
@@ -20,9 +20,19 @@ echo "# Interaction energies in kcal/mol"
 # --- Extract energy table ---
 awk '
 /^===/ { block = (block + 1) % 2; next }
-block && /^[0-9]/ {
+block && /^[[:space:]]*[0-9]/ {
+    # find first numeric field (supports 1.23, -4.5, 6e-3, -7.8E+2, etc.)
+    firstnum = 0
+    for (i = 2; i <= NF; i++) {
+        if ($i ~ /^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$/) { firstnum = i; break }
+    }
+    if (!firstnum) next  # no numeric found on this line
+
+    # name is everything between the index ($1) and the first numeric
     name = $2
-    energy = $3
+    for (j = 3; j < firstnum; j++) name = name " " $j
+
+    energy = $firstnum
     total_width = 32
     printf "%-*s%*.3f\n", total_width - 8, name, 8, energy
 }' "$LOG_FILE"
